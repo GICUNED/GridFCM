a3a4b9c40a487b044c965cb1c182a6597e6284b0
Asanfe <sanfeliciano.alejandro@gmail.com>
2021-05-18 23:17:48 +0200

    Funciones de mapas

--- R/FCMfunctions.R
+++ R/FCMfunctions.R
@@ -5,6 +5,25 @@
 ################################################################################
 SelectConstructs <- function(x){
 
+   #Adaptamos el formato de OpenRepGrid a una matriz estandar.
+  if(class(x)[1] == "repgrid"){
+    x <- .adaptrepgrid(x)
+  }
+
+  constructs <- colnames(x)
+  constructs <- as.matrix(constructs, ncol = 1)
+  cat("############################################################## \n")
+  cat("SELECCIONA LOS CONSTRUCTOS QUE DESEAS TENER EN TU NUEVA MATRIZ \n")
+  cat("############################################################## \n")
+  print(constructs)
+  constructs.selected <- scan(what = "numeric", nmax = dim(x)[2])
+
+  constructs.selected <- as.vector(constructs.selected)
+  constructs.selected <- as.numeric(constructs.selected)
+
+    result <- x[constructs.selected,constructs.selected]
+
+    return(result)
 }
 ################################################################################
 WeightMatrix <- function(x,imp){
