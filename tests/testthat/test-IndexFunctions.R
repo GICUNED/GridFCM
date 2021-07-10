0a67a8ec5f0def704b5ea88dfb7769bbd1b2c391
Asanfe <sanfeliciano.alejandro@gmail.com>
2021-07-10 14:54:46 +0200

    Arreglos en test automático

--- tests/testthat/test-IndexFunctions.R
+++ tests/testthat/test-IndexFunctions.R
@@ -2,36 +2,36 @@
 matrix <- cbind(c(0,0,0,1),c(1,0,0,0),c(0,1,0,0),c(0,0,1,0))
 
 test_that("CentDegree", {                                                       # Test de resultados para la función CentDegree con todos los métodos
-  expect_equal(as.vector(CentDegree(impgrid_01)$Entrada),c(5,2,7,5,3,4,2,8,5,7))
+  expect_equal(as.vector(CentDegree(impgrid_01)$Salidas),c(5,2,7,5,3,4,2,8,5,7))
 
-  expect_equal(as.vector(CentDegree(impgrid_01)$Salida),c(5,5,7,4,5,3,3,5,5,6))
+  expect_equal(as.vector(CentDegree(impgrid_01)$Entradas),c(5,5,7,4,5,3,3,5,5,6))
 
-  expect_equal(as.vector(CentDegree(impgrid_01, method = "weight")$Entrada),
+  expect_equal(as.vector(CentDegree(impgrid_01, method = "weight")$Salidas),
                c(2.330,0.667,3.667,3.667,2,2,1,4.330,1.667,4.667),
                tolerance = 0.001)
 
-  expect_equal(as.vector(CentDegree(impgrid_01, method = "weight")$Salida),
+  expect_equal(as.vector(CentDegree(impgrid_01, method = "weight")$Entradas),
                c(4,1.667,4.667,2,2,1.333,1.333,2.667,2.667,3.667),
                tolerance = 0.001)
 
-  expect_equal(as.vector(CentDegree(impgrid_01, method = "wnorm")$Entrada),
+  expect_equal(as.vector(CentDegree(impgrid_01, method = "wnorm")$Salidas),
                c(2.330,0.667,3.667,3.667,2,2,1,4.330,1.667,4.667)/9,
                tolerance = 0.001)
 
-  expect_equal(as.vector(CentDegree(impgrid_01, method = "wnorm")$Salida),
+  expect_equal(as.vector(CentDegree(impgrid_01, method = "wnorm")$Entradas),
                c(4,1.667,4.667,2,2,1.333,1.333,2.667,2.667,3.667)/9,
                tolerance = 0.001)
 
-  expect_equal(as.vector(CentDegree(impgrid_01, method = "norm")$Entrada),
+  expect_equal(as.vector(CentDegree(impgrid_01, method = "norm")$Salidas),
                c(5,2,7,5,3,4,2,8,5,7)/9)
 
-  expect_equal(as.vector(CentDegree(impgrid_01, method = "norm")$Salida),
+  expect_equal(as.vector(CentDegree(impgrid_01, method = "norm")$Entradas),
                c(5,5,7,4,5,3,3,5,5,6)/9)
 
-  expect_equal(as.vector(CentDegree(impgrid_01, method = "ego")$Entrada),
+  expect_equal(as.vector(CentDegree(impgrid_01, method = "ego")$Salidas),
                c(5,2,7,5,3,4,2,8,5,7)/(9*10))
 
-  expect_equal(as.vector(CentDegree(impgrid_01, method = "ego")$Salida),
+  expect_equal(as.vector(CentDegree(impgrid_01, method = "ego")$Entradas),
                c(5,5,7,4,5,3,3,5,5,6)/(9*10))
 
 })
